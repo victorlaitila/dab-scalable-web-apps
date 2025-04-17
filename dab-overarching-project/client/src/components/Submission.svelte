d72e7345e5509a934ee22b4c4e065a120622cba2
Victor Laitila <victorlaitila@MacBook-Pro-som-tillhor-Victor.local>
2025-04-17 17:08:12 +0300

    Working client with Svelte

---
+++ dab-overarching-project/client/src/components/Submission.svelte
@@ -0,0 +1,26 @@
+<script>
+	let inputString = $state('');
+	let numberOfCharacters = $state(null);
+	let numberOfIfs = $state(null);
+	
+  const handleSubmit = () => {
+    if (inputString) {
+      numberOfCharacters = inputString.length;
+			numberOfIfs = (inputString.match(/if/g) || []).length;
+    }
+  };
+</script>
+
+<div>
+	<textarea rows="4" cols="50" bind:value={inputString}></textarea>
+</div>
+<div>
+	<button onclick={handleSubmit}>Submit</button>	
+</div>
+
+{#if numberOfCharacters !== null}
+  <p>Characters: {numberOfCharacters}</p>
+{/if}
+{#if numberOfIfs !== null}
+  <p>ifs: {numberOfIfs}</p>
+{/if}
